3993509c98f0c12b6fa353bb27f7dbc51b0bdd9e
opossum-3 <konstantin.nikolov.yuriev@gmail.com>
2025-03-15 11:59:06 +0200

    Factors class added

---
+++ Models/Factors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public static class Factors
+    {
+        public static double WindSpeed {  get; set; }
+        public static double WindDirection { get; set; }
+        public static double Temperature { get; set; }
+    }
+}
