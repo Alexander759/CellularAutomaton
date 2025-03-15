c0bb1c4b8ee28303237e53eab202a6d8be03ca8b
Kristian Hristakiev <khristakiev@gmail.com>
2025-03-15 21:34:08 +0200

    add canvasResize

---
+++ CellularAutomaton.Web/Models/DrawModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CellularAutomaton.Web.Models
+{
+    public class DrawModel
+    {
+        [Required]
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
