fcf87e2ed9172ab3f45f52a69ede942cd254ff1c
Alexander759 <aleks.asenov2005@gmail.com>
2025-03-15 17:00:43 +0200

    Add Image API Controller

---
+++ CellularAutomaton.Web/Controllers/ImagesAnimationController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CellularAutomaton.Web.Controllers
+{
+    public class ImageRequest
+    {
+        public string Image { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesAnimationController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public ImagesAnimationController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadImage([FromBody] ImageRequest? imageRequest)
+        {
+            try
+            {
+                var base64Data = imageRequest.Image.Replace("data:image/png;base64,", "");
+
+                byte[] imageBytes = Convert.FromBase64String(base64Data);
+
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", "image.png");
+
+                var directory = Path.GetDirectoryName(filePath);
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                await System.IO.File.WriteAllBytesAsync(filePath, imageBytes);
+
+                return Ok(new { message = "Image uploaded successfully" });
+            }
+            catch (Exception ex)
+            {
+                // Return an error response
+                return BadRequest(new { message = "Error uploading image", details = ex.Message });
+            }
+        }
+    }
+
+    
+}
