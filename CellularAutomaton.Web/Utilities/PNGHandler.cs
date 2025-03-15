5075c94fee41ede0f3e579fdfbd02fdfd67cbd6a
smeshen-2 <140281398+smeshen-2@users.noreply.github.com>
2025-03-15 16:35:25 +0200

    Change location of utilities, scaling tiles

---
+++ CellularAutomaton.Web/Utilities/PNGHandler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+
+namespace Utilities
+{
+	static class PNGHandler
+	{
+		public static int width, height, tileSize = 10;
+		public static Tile[,] Read(string filePath)
+		{
+			using SKBitmap bitmap = SKBitmap.Decode(filePath);
+			width = bitmap.Width;
+			height = bitmap.Height;
+			Console.WriteLine($"Image loaded: {width}x{height}");
+			Tile[,] tiles = new Tile[width / tileSize, height / tileSize];
+
+			for (int i = 0; i < tiles.GetLength(0); i++)
+			{
+				for (int j = 0; j < tiles.GetLength(1); j++)
+				{
+					var tuple = Tile.fromColor[bitmap.GetPixel(i * tileSize, j * tileSize)];
+					if (tuple.Item2 == DensityType.None) tiles[i, j] = new Tile(tuple.Item1, tuple.Item2, BurnStateType.None);
+					else tiles[i, j] = new Tile(tuple.Item1, tuple.Item2, BurnStateType.Fuel);
+				}
+			}
+			return tiles;
+		}
+		
+		public static void Write(Tile[,] tiles, string filePath, int index)
+		{
+			using SKBitmap bitmap = new SKBitmap(width, height);
+			using SKCanvas canvas = new SKCanvas(bitmap);
+			IntPtr pixelsPtr = bitmap.GetPixels();
+
+			if (pixelsPtr == IntPtr.Zero)
+				throw new Exception("Failed to get pixel buffer!");
+
+			// Fill the pixel array (Set each pixel manually)
+			for (int x = 0; x < width / tileSize; x++)
+			{
+				for (int y = 0; y < height / tileSize; y++)
+				{
+					SKPaint paint;
+					if (tiles[x, y].BurnState == BurnStateType.Burning)
+					{
+						paint = new SKPaint { Color = SKColors.Red };
+					}
+					else if (tiles[x, y].BurnState == BurnStateType.Burnt)
+					{
+						paint = new SKPaint { Color = SKColors.Brown };
+					}
+					else {
+
+						paint = new SKPaint {Color = Tile.toColors[
+							((VegetationType)(tiles[x, y].Vegetation * 10),
+							(DensityType)(tiles[x, y].Density * 10))]
+						};
+					}
+					canvas.DrawRect(x * tileSize, y * tileSize, tileSize, tileSize, paint);
+				}
+			}
+
+			// Encode as PNG
+			using var image = SKImage.FromBitmap(bitmap);
+			using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+
+			// Save the file
+			File.WriteAllBytes(filePath + $"\\output\\{index}.png", data.ToArray());
+
+			Console.WriteLine($"PNG image saved as {index}.png");
+		}
+	}
+}
---
+++ CellularAutomaton.Web/Utilities/Tile.cs
@@ -0,0 +1,85 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+	public enum VegetationType : sbyte
+	{
+		Water = -20,
+		Rock = -10,
+		Low = -3,
+		Medium = 0,
+		High = 4
+	}
+
+	public enum DensityType : sbyte
+	{
+		None = -10,
+		Sparse = -4,
+		Medium = 0,
+		Dense = 3
+	}
+
+	public enum BurnStateType : byte
+	{
+		None,
+		Fuel,
+		Burning,
+		Burnt
+	}
+
+	public struct Tile
+	{
+		public Tile(VegetationType vegetation, DensityType density, BurnStateType burnState)
+		{
+			Vegetation = (double)vegetation / 10;
+			Density = (double)density / 10;
+			BurnState = burnState;
+		}
+
+		public double Vegetation { get; private set; }
+		public double Density { get; private set; }
+		public BurnStateType BurnState { get; set; }
+
+		public static Dictionary<(VegetationType, DensityType), SKColor> toColors = new()
+		{
+			{ (VegetationType.Water, DensityType.None), SKColors.CornflowerBlue },
+			{ (VegetationType.Rock, DensityType.None), SKColors.Gray },
+
+			{ (VegetationType.Low, DensityType.Sparse), SKColors.Yellow },
+			{ (VegetationType.Medium, DensityType.Sparse), SKColors.YellowGreen },
+			{ (VegetationType.High, DensityType.Sparse), SKColors.GreenYellow },
+
+			{ (VegetationType.Low, DensityType.Medium), SKColors.LightGreen },
+			{ (VegetationType.Medium, DensityType.Medium), SKColors.Lime },
+			{ (VegetationType.High, DensityType.Medium), SKColors.Green },
+
+			{ (VegetationType.Low, DensityType.Dense), SKColors.Teal },
+			{ (VegetationType.Medium, DensityType.Dense), SKColors.ForestGreen },
+			{ (VegetationType.High, DensityType.Dense), SKColors.DarkGreen },
+		};
+
+		public static Dictionary<SKColor, (VegetationType, DensityType)> fromColor = new()
+		{
+			{ SKColors.CornflowerBlue, (VegetationType.Water, DensityType.None) },
+			{ SKColors.Gray, (VegetationType.Rock, DensityType.None) },
+
+			{ SKColors.Yellow , (VegetationType.Low, DensityType.Sparse) },
+			{ SKColors.YellowGreen , (VegetationType.Medium, DensityType.Sparse) },
+			{ SKColors.GreenYellow , (VegetationType.High, DensityType.Sparse) },
+
+			{ SKColors.LightGreen , (VegetationType.Low, DensityType.Medium) },
+			{ SKColors.Lime , (VegetationType.Medium, DensityType.Medium) },
+			{ SKColors.Green , (VegetationType.High, DensityType.Medium) },
+
+			{ SKColors.Teal , (VegetationType.Low, DensityType.Dense) },
+			{ SKColors.ForestGreen , (VegetationType.Medium, DensityType.Dense) },
+			{ SKColors.DarkGreen , (VegetationType.High, DensityType.Dense) },
+		};
+
+	}
+}
